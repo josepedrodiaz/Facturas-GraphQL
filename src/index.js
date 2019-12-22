6c1d38beeb2a6d2514b7c9dad5882e42af6d37b1
Pedro D <josepedrodiaz@gmail.com>
2019-12-22 13:34:20 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const Query = require('./resolvers/Query')
 const Mutation = require('./resolvers/Mutation')
 const User = require('./resolvers/User')
-const Link = require('./resolvers/Factura')
+const Factura = require('./resolvers/Factura')
 
 const { GraphQLServer } = require('graphql-yoga')
 const { prisma } = require('./generated/prisma-client')
@@ -11,7 +11,7 @@ const resolvers = {
   Query,
   Mutation,
   User,
-  Link
+  Factura
 }
 
 // 3
