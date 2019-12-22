f3ed3d960903bf6bfb1a5cc1d9688933ee9775a3
Pedro D <josepedrodiaz@gmail.com>
2019-12-22 10:19:10 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,53 +11,54 @@ const resolvers = {
     },
     getFacturaByPeriodo: (root, args, context, info) => {
       return context.prisma.facturas({ periodo: args.periodo })
+    }
+  },
+
+  Mutation: {
+    post: (root, args, context) => {
+      return context.prisma.createFactura({
+        periodo: args.periodo,
+        monto: args.monto,
+      })
     },
 
-    Mutation: {
-      post: (root, args, context) => {
-        return context.prisma.createFactura({
-          periodo: args.periodo,
-          monto: args.monto,
-        })
-      },
-
-      put: (parent, args) => {
-        let facturaActualizadaIndex
-        for (let i = 0, len = facturas.length; i < len; i++) {
-          if (facturas[i]["id"] === args.id) {
-            facturas[i].periodo = args.periodo
-            facturas[i].monto = args.monto
-            facturaActualizadaIndex = i
-            break
-          }
+    put: (parent, args) => {
+      let facturaActualizadaIndex
+      for (let i = 0, len = facturas.length; i < len; i++) {
+        if (facturas[i]["id"] === args.id) {
+          facturas[i].periodo = args.periodo
+          facturas[i].monto = args.monto
+          facturaActualizadaIndex = i
+          break
         }
-        return facturas[facturaActualizadaIndex]
+      }
+      return facturas[facturaActualizadaIndex]
 
-      },
+    },
 
-      delete: (parent, args) => {
-        let elementosBorrados = false
-        for (let i = 0, len = facturas.length; i < len; i++) {
-          if (facturas[i]["periodo"] === args.periodo) {
-            facturas.splice(i, 1);
-            elementosBorrados = true
-          }
+    delete: (parent, args) => {
+      let elementosBorrados = false
+      for (let i = 0, len = facturas.length; i < len; i++) {
+        if (facturas[i]["periodo"] === args.periodo) {
+          facturas.splice(i, 1);
+          elementosBorrados = true
         }
-        if (elementosBorrados) {
-          return "Se han borrado todas las facturas correspondientes al período " + args.periodo
-        } else {
-          return "No se encuentran facturas correspondientes al período " + args.periodo
-        }
+      }
+      if (elementosBorrados) {
+        return "Se han borrado todas las facturas correspondientes al período " + args.periodo
+      } else {
+        return "No se encuentran facturas correspondientes al período " + args.periodo
       }
     }
   }
+}
 
 // 3
 const server = new GraphQLServer({
-    typeDefs: './src/schema.graphql',
-    resolvers,
-    context: { prisma },
-  })
+  typeDefs: './src/schema.graphql',
+  resolvers,
+  context: { prisma },
+})
 
 server.start(() => console.log(`Server is running on http://localhost:4000`))
 
