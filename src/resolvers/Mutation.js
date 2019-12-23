2be4b0bf767d8e06b8a351b532028b32580e6769
Pedro D <josepedrodiaz@gmail.com>
2019-12-23 00:53:27 -0300

    Retoques de JWT Expiration

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -3,15 +3,13 @@ const jwt = require('jsonwebtoken')
 const { APP_SECRET, getUserId } = require('../utils')
 
 async function signup(parent, args, context, info) {
-    // 1
+  
     const password = await bcrypt.hash(args.password, 10)
-    // 2
+  
     const user = await context.prisma.createUser({ ...args, password })
   
-    // 3
     const token = jwt.sign({ userId: user.id }, APP_SECRET)
   
-    // 4
     return {
       token,
       user,
@@ -19,27 +17,20 @@ async function signup(parent, args, context, info) {
   }
   
   async function login(parent, args, context, info) {
-    // 1
+
     const user = await context.prisma.user({ email: args.email })
     if (!user) {
       throw new Error('No such user found')
     }
   
-    // 2
     const valid = await bcrypt.compare(args.password, user.password)
     if (!valid) {
       throw new Error('Invalid password')
     }
-  
-    const token = jwt.sign({ exp: (Math.floor(Date.now() / 1000) + 30), userId: user.id }, APP_SECRET)
+    
+    let expirationDate = (Math.floor(Date.now() / 1000) + (60 * 25)) //25'
+    const token = jwt.sign({ exp: expirationDate, userId: user.id }, APP_SECRET)
 
-    // const token = ({
-    //   //exp: Math.floor(Date.now() / 1000) + (60 * 60),
-    //   exp: Math.floor(Date.now() / 1000) + 10,//10 seconds expiration
-    //   userId: user.id
-    // }, APP_SECRET);
-  
-    // 3
     return {
       token,
       user,
